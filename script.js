87554eec29a5ca824544059fb89883ec4a9b9c02
Rabeya-Khatun1 <prorabeya2008@gmail.com>
2026-03-06 10:04:11 +0600

    added one more features

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-// function calculate() {
-//   let num1 = parseFloat(document.getElementById("num1").value);
-//   let num2 = parseFloat(document.getElementById("num2").value);
-//   let operator = document.getElementById("operator").value;
-//   let result;
-
-//   if (operator === "+") result = num1 + num2;
-//   else if (operator === "-") result = num1 - num2;
-//   else if (operator === "*") result = num1 * num2;
-//   else if (operator === "/") result = num1 / num2;
-//   else result = "Invalid operator";
-
-//   document.getElementById("result").innerText = "Result: " + result;
-// }
-
 function append(value) {
-  document.getElementById("display").value += value;
+  let display = document.getElementById("display");
+
+  if (display.value === "0" && value !== "." && isNaN(value) === false) {
+    display.value = value; 
+  } else {
+    display.value += value; 
+  }
 }
 
 function clearDisplay() {
-  document.getElementById("display").value = "";
+  document.getElementById("display").value = "0";
 }
 function backspace() {
   let display = document.getElementById("display");
   display.value = display.value.slice(0, -1);
+   if (display.value === "") {
+    display.value = "0";
+  }
 }
 
 function calculate() {
